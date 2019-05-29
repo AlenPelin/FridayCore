30d29ee5179940fde05faf6d6ad6cf184272e74a
Alen Pelin <alen.pelin@gmail.com>
2019-05-29 18:07:14 +1000

    Keep only XA due to extracting it to separate branch

--- src/FridayCore.Common/Properties/InternalsVisibleTo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("FridayCore.AccountResetRules")]
-[assembly: InternalsVisibleTo("FridayCore.AutoPackages")]
-[assembly: InternalsVisibleTo("FridayCore.ConfigExtensions")]
-[assembly: InternalsVisibleTo("FridayCore.SignUpRules")]
-[assembly: InternalsVisibleTo("FridayCore.SitecoreStarted")]
--- src/FridayCore.ConfigExtensions/Configuration/ConfigExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace FridayCore.Configuration
-{
-  internal static class ConfigExtensions
-  {
-    internal const string FeatureName = "Config Extensions";
-
-    internal static bool Enabled { get; set; }
-  }
-}
