250334909722a3badfc8f012f5d3c01dd9577aa1
Alen Pelin <alen.pelin@gmail.com>
2019-05-29 18:03:54 +1000

    Remove XA due to extracting it to separate branch

--- src/FridayCore.XA/Properties/VersionInfo.cs
+++
@@ -1,5 +0,0 @@
-// DO NOT CHANGE
-
-[assembly: System.Reflection.AssemblyVersion             ("0.0.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion         ("0.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("0.0.0.0")]
